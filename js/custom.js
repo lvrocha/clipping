83c16d33871ea748a7d904585678330d5e034e3a
Lucas Vilhena <contato@lucasvilhena.com.br>
2020-02-06 15:17:50 -0300

    listagem de cursos ok

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,17 @@ $(function(){
     const url = 'https://www.codepile.net/raw/odNvjOdO.js';
     $.getJSON(url).done(function(data){
         console.log(data);
+        $.each(data, function(i, item){
+            let name = item.name;
+            let name_td = $('<td>').append(name);
+            let price = item.price;
+            let price_td = $('<td>').append(price);
+            let tr1 = $('<tr>').html("").append(name_td, price_td);
+            tr1.appendTo("#courses");
+        });
+    }).fail(function(){
+        console.log("Requisição falhou");
+    }).always(function(){
+        console.log('Requisição Ok')
     })
 })
